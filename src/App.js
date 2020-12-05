afe5b92543e8180619125ca36f2642ff62bba224
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-12-05 15:13:19 -0500

    Fix document reference error

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ const App = () => {
   const [page, setPage] = useState(0);
 
   const onClick = () => {
-    const article = new Readability(document).parse();
-    const text = new DOMParser()
-      .parseFromString(article.content, "text/html")
-      .body.innerText.replace(/\s+/g, " ");
-    // Generate a word array
-    // There shouldn't be any words over 31 letters so those are usually mistakes
-    const wordArr = text
-      .split(" ")
-      .filter((word) => word.length > 0 && word.length < 31);
+    try {
+      const clonedDom = document.cloneNode(true);
+      const article = new Readability(clonedDom).parse();
+      const text = new DOMParser()
+        .parseFromString(article.content, "text/html")
+        .body.innerText.replace(/\s+/g, " ");
+      // Generate a word array
+      // There shouldn't be any words over 31 letters so those are usually mistakes
+      const wordArr = text
+        .split(" ")
+        .filter((word) => word.length > 0 && word.length < 31);
+    } catch (err) {
+      // TODO: Add better error handling
+      console.log(err);
+    }
   };
 
   return (
@@ -24,7 +30,6 @@ const App = () => {
       <h1>Type Racer</h1>
       <p>Learn to type effortlessly</p>
       <button onClick={onClick}></button>
-      <p>{page}</p>
     </div>
   );
 };
